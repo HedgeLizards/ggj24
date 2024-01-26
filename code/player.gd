fafd880ebebe09c89af1191eae39734d7ef010ae
troido <troido@protonmail.com>
2024-01-26 19:50:19 +0100

    2 bouncy balls

--- code/player.gd
+++ code/player.gd
@@ -1,7 +1,11 @@
 extends RigidBody3D
 
-const speed = 100
+@export var player_id: int = 0
+const speed: float = 1000
+
+
 
 func _integrate_forces(state):
-	var movement = Input.get_vector("left", "right", "up", "down") * speed
+	var current_speed: float = Vector2(linear_velocity.x, linear_velocity.z).length()
+	var movement: Vector2 = PlayerInput.movement_vector(player_id) * speed / (1 + current_speed)
 	apply_force(Vector3(movement.x, 0, movement.y))
