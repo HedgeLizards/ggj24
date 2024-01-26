d97d1b847e1c4642b15c819cc0560162e4f6e2f6
Julian Vos <julianvosje@gmail.com>
2024-01-26 20:26:29 +0100

    Add basic UI and InputHandler

--- code/player.gd
+++ code/player.gd
@@ -7,5 +7,5 @@ const speed: float = 1000
 
 func _integrate_forces(state):
 	var current_speed: float = Vector2(linear_velocity.x, linear_velocity.z).length()
-	var movement: Vector2 = PlayerInput.movement_vector(player_id) * speed / (1 + current_speed)
+	var movement: Vector2 = InputHandler.movement_vector(player_id) * speed / (1 + current_speed)
 	apply_force(Vector3(movement.x, 0, movement.y))
