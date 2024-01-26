ec794fe778e1d35dea7c4ed7bef537c875138a46
troido <troido@protonmail.com>
2024-01-26 20:44:00 +0100

    bigger walls and different movement

--- code/player.gd
+++ code/player.gd
@@ -1,11 +1,16 @@
 extends RigidBody3D
 
 @export var player_id: int = 0
-const speed: float = 1000
-
+const speed: float = 100
+const max_speed: float = 100
 
 
 func _integrate_forces(state):
-	var current_speed: float = Vector2(linear_velocity.x, linear_velocity.z).length()
-	var movement: Vector2 = PlayerInput.movement_vector(player_id) * speed / (1 + current_speed)
+	var current_speed := Vector2(linear_velocity.x, linear_velocity.z)
+	var movement: Vector2 = PlayerInput.movement_vector(player_id) * speed
+	if movement.x < 0 && current_speed.x < -max_speed || movement.x > 0 && current_speed.x > max_speed:
+		movement.x = 0
+	if movement.y < 0 && current_speed.y < -max_speed || movement.y > 0 && current_speed.y > max_speed:
+		movement.y = 0
+		
 	apply_force(Vector3(movement.x, 0, movement.y))
