eb1be5d34b2b957c617b3dab7bd63aa89a2f7875
Julian Vos <julianvosje@gmail.com>
2024-01-27 00:02:07 +0100

    Add basic Start scene

---
+++ code/start.gd
@@ -0,0 +1,28 @@
+extends Control
+
+func _ready():
+	var screen_scale = DisplayServer.screen_get_scale()
+	
+	$VBoxContainer.add_theme_constant_override('separation',
+		$VBoxContainer.get_theme_constant('separation') * screen_scale
+	)
+	
+	for button in [$VBoxContainer/Play, $VBoxContainer/Quit]:
+		button.add_theme_font_size_override('font_size',
+			button.get_theme_font_size('font_size') * screen_scale
+		)
+	
+	for texture in [$Logo, $Audio]:
+		texture.scale *= screen_scale
+	
+	if OS.has_feature('web'):
+		$VBoxContainer/Quit.visible = false
+
+func _on_play_pressed():
+	get_tree().change_scene_to_packed(preload('res://scenes/main.tscn'))
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_audio_toggled(toggled_on):
+	AudioServer.set_bus_mute(0, toggled_on)
