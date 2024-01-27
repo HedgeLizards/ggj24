107c2e87528242ca97b740ca593fc5587c9a5968
Milan Raphaël <MilanTent@live.nl>
2024-01-27 23:45:32 +0100

    Added Join/Leave SFX

--- code/main.gd
+++ code/main.gd
@@ -59,6 +59,12 @@ func _on_player_joined(id):
 	#$Level/Lobby/Spawns.get_children().pick_random()
 	cat.go_to($Level/Lobby/Spawns.get_children().pick_random().position)
 	update_join_notice()
+	
+	# Sound Effects
+	$SoundEffects/SND_Player_Joined.play();
+	$SoundEffects/PlayerJoined.get_child(id).play();
+	
+	
 
 func _on_in_game_body_exited(body):
 	if state == State.LOBBY:
@@ -98,6 +104,7 @@ func _on_lobby_player_leave(player):
 	player.queue_free()
 	update_join_notice()
 	_on_lobby_player_ready.call_deferred(null)
+	$SoundEffects/SND_Player_Left.play();
 
 func start_lobby():
 	InputHandler.can_add_players = true
