0fcaf59a2a7bf8926875dd4051587e06f2f22420
troido <troido@protonmail.com>
2024-01-27 17:59:02 +0100

    more push when moving faster

--- code/player.gd
+++ code/player.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 const speed: float = 100
 const max_speed: float = 100
 const jump: float = 30
-const superbounce: float = 10000
+const superbounce: float = 500
 
 func is_on_floor():
 	return %FloorCheck.get_overlapping_bodies().any(func(body): return body is StaticBody3D)
@@ -26,7 +26,7 @@ func _integrate_forces(_state):
 
 func _on_body_entered(body):
 	if body.has_method("is_player"):
-		apply_force(-(body.global_position - global_position).normalized()* superbounce)
+		apply_force(-(body.global_position - global_position).normalized()* superbounce * body.linear_velocity.length())
 	#print("bounce")
 
 func is_player():
