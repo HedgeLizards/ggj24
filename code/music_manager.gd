287963e446ae3197ce58e2b33634402d28bb1ba3
Julian Vos <julianvosje@gmail.com>
2024-01-27 22:58:49 +0100

    Remove warnings and print statements

--- code/music_manager.gd
+++ code/music_manager.gd
@@ -1,22 +1,10 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_mus_lobby_finished():
 	if get_parent().state == get_parent().State.STARTING:
 		$MUS_Transition_1.play();
 	else:
 		$MUS_Lobby.play();
-
 
 func _on_mus_transition_1_finished():
 	$MUS_Game_Time_Loop.play();
